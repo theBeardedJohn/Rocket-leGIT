f8bc5c9eeeae4804d157181afcfe4f4718da420d
theBeardedJohn <120687119+theBeardedJohn@users.noreply.github.com>
2023-01-06 16:32:16 +0100

    WorkingCameraScript

--- Assets/CameraTracking.cs
+++ Assets/CameraTracking.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraTracking : MonoBehaviour
@@ -8,19 +9,33 @@ public class CameraTracking : MonoBehaviour
 
     public Transform _rocket;
     public float _cameraZoom;
-
+    [SerializeField] float _cameraZoomSensitivity;
     private float _cameraZoomVectorZ; // hodnota Z vektoru pro _cameraZoomVectorZ
     private Vector3 _cameraZoomVector; // plovoucí vector3 hodnota pricitana k offsetu
     [SerializeField] Vector3 _cameraOffset; // preddefinovany offset pozice (vector3) camery
-    public float _speed;
 
-   
-    
+
+
+
+    //pristup k _speed z jineho scriptu
+    public GameObject _rocketEmpty; //definované PUBLIC pole pro pøiøazení GameObjektu 
+    private Movement _movementScript;
+    public float _localSpeed;
+    public float _speed;
+    //pristup k _speed z jineho scriptu
+
+
+
+
     void Start()
     {
+        _cameraZoomSensitivity = 3f;
+        _movementScript = _rocketEmpty.GetComponent<Movement>();
+
         _cameraOffset = new Vector3(0, 4, -15);
-        _cameraZoom = 1;
-    
+        _cameraZoom = -1;
+      
+
     }
    
     void Update()
@@ -28,11 +43,27 @@ public class CameraTracking : MonoBehaviour
 
 
 
+        _localSpeed = _movementScript._speed;
 
-        _cameraZoomVectorZ = Mathf.Clamp(_cameraZoom, 0, 15);
+        if (_localSpeed < 10)
+        {
+            _cameraZoom += (_cameraZoomSensitivity/100);
+            _cameraZoom = Mathf.Clamp(_cameraZoom, -20, 0);
+        }
+
+        else
+        {
+            _cameraZoom -= (_cameraZoomSensitivity / 100);
+            _cameraZoom = Mathf.Clamp(_cameraZoom, -20, 0);
+        }
+
+
+
+        _cameraZoomVectorZ = _cameraZoom;
         _cameraZoomVector = new Vector3 (0, 0, _cameraZoomVectorZ);
         transform.position = _rocket.position + _cameraOffset + _cameraZoomVector;
 
-        Debug.Log("rychlost speed" + _speed);
+        Debug.Log("rychlost speed" + _localSpeed);
+        Debug.Log("_cameraZoom" + _cameraZoom);
     }
 }
